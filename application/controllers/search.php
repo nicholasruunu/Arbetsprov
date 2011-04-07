f8f3f43595de5983751ac4f04eb5ad0b9ed15d08
Nicholas Ruunu <nicholas.ruunu@gmail.com>
2011-04-07 19:48:53 +0200

    Made the search-controller easier to read

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -16,6 +16,10 @@ class Search extends CI_Controller{
 		// Initierar data, urldecodar om man valt att söka från URL eller utan javascript (submit)
 		$site			= $this->data['site']			= $this->uri->rsegment(2, 'all');
 		$search_string	= $this->data['search_string']	= urldecode(trim($this->uri->rsegment(3, '')));
+		
+		// Om siten inte finns i config/search_settings.php gå till base_url
+		if( ! (array_key_exists($site, $this->search_model->search_engines) || $site === 'all'))
+			redirect("/");
 			
 		// Ser om vi postat utan javascript enablat
 		if($this->input->post('submit') !== FALSE)
@@ -30,12 +34,11 @@ class Search extends CI_Controller{
 		);
 		
 		// Sätter aktivt menyval
-		if(array_key_exists($site, $this->data['menu_array']))
-			$this->data['menu_array'][$site]['active'] = TRUE;
+		$this->data['menu_array'][$site]['active'] = TRUE;
 		
 		// Gör en sökning om vi har någonting i söksträngen
 		if($search_string !== '')
-			$this->search($site, $search_string);
+			$this->data['search_results']['results'] = $this->search($site, $search_string);
 
 		// Echoar ut sökresultaten vi requestar med ajax
 		if($this->input->is_ajax_request())
@@ -46,16 +49,9 @@ class Search extends CI_Controller{
 	
 	private function search($site, $search_string)
 	{
-		if(array_key_exists($site, $this->search_model->search_engines) || $site === 'all')
-		{			
-			$this->data['search_results']['results'] = ($site === 'all')
-				? $this->search_model->get_combined_results($search_string)
-				: $this->search_model->get_results($site, $search_string);
-		}
-		else
-		{
-			show_error('Sökmotorn existerar inte!');
-		}
+		return ($site === 'all')
+			? $this->search_model->get_combined_results($search_string)
+			: $this->search_model->get_results($site, $search_string);
 	}
 	
 	private function submit($site)
@@ -63,7 +59,7 @@ class Search extends CI_Controller{
 		$search_string = ($this->input->post('search_string') !== FALSE)
 			? urlencode($this->input->post('search_string')) : '';
 		
-		// Redirectar för att URL-strukturen ska bli snygg utan javascript
+		// Redirectar för att URL-strukturen ska bli snygg och delbar utan javascript
 		redirect("/search/{$site}/{$search_string}");
 	}
 }
