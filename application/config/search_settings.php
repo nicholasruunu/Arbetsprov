c98f2c944258e278e01f3f0d57f68967a44c658e
Nicholas Ruunu <nicholas.ruunu@gmail.com>
2011-04-07 18:35:55 +0200

    Tidying up

--- application/config/search_settings.php
+++ application/config/search_settings.php
@@ -6,14 +6,23 @@
 			// Ändrade från google.com till google.se eftersom min browser (FF 4.0) ändrade resultaten till det automatiskt
 			// medans cURL kom förbi, antog att era gör detsamma så ändrade för att få konsekventa resultat
 			'url' => 'http://www.google.se/search?as_qdr=all&num=20&q=',
-			'regex' => '/<h3\sclass="?r"?><a href="(?P<url>http:\/\/[^"]+?)".*?>(?P<title>[^"\n\r\t]+?)<\/a><\/h3>(?:<span\sclass="?std\snobr"?>.*?<\/span>)?(?:<div\sclass="?s"?>(<div\sclass="?f"?>.*?<\/div>)?(?P<text>.*?)(<br>.*?)?)?<span\sclass="?f"?>/m'
+			'regex' => '/<h3\sclass="?r"?><a href="'.
+				'(?P<url>http:\/\/[^"]+?)".*?>'.
+				'(?P<title>[^"\n\r\t]+?)<\/a><\/h3>(?:<span\sclass="?std\snobr"?>.*?<\/span>)?(?:<div\sclass="?s"?>(<div\sclass="?f"?>.*?<\/div>)?'.
+				'(?P<text>.*?)(<br>.*?)?)?<span\sclass="?f"?>/m'
 		),
 		'bing' => array(
 			'url' => 'http://www.bing.com/search?q=',
-			'regex' => '/<div\sclass="?sb_tlst"?><h3><a.+?href="(?P<url>http:\/\/[^"]+?)".*?>(?P<title>[^\n\r\t]+?)<\/a>.*?(?:<p>(?P<text>.*?)<\/p>)?<div\sclass="sb_meta">/m'
+			'regex' => '/<div\sclass="?sb_tlst"?><h3><a.+?href="'.
+				'(?P<url>http:\/\/[^"]+?)".*?>'.
+				'(?P<title>[^\n\r\t]+?)<\/a>.*?(?:<p>'.
+				'(?P<text>.*?)<\/p>)?<div\sclass="sb_meta">/m'
 		),
 		'yahoo' => array(
 			'url' => 'http://search.yahoo.com/search?n=20&p=',
-			'regex' => '/<div\sclass="?res"?><div>(?:\s<span\sclass="?ext"?>.*?<\/span>\s)?<h3><a.+?href="(?P<url>http:\/\/[^"]+?)".*?>(?P<title>[^\n\r\t]+?)<\/a>.*?(?:<div\sclass="?(sm-abs|abstr)"?>(?P<text>.*?)<\/div>.*?)?<span\sclass="?url"?>/m'
+			'regex' => '/<div\sclass="?res"?><div>(?:\s<span\sclass="?ext"?>.*?<\/span>\s)?<h3><a.+?href="'.
+				'(?P<url>http:\/\/[^"]+?)".*?>'.
+				'(?P<title>[^\n\r\t]+?)<\/a>.*?(?:<div\sclass="?(sm-abs|abstr)"?>'.
+				'(?P<text>.*?)<\/div>.*?)?<span\sclass="?url"?>/m'
 		),
 	);
