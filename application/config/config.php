7827c098bdb5860ad60082c9726931e3d5605421
Nicholas Ruunu <nicholas.ruunu@gmail.com>
2011-05-23 20:20:44 +0200

    Updated info for deployment

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://nicholas.no-ip.org/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
